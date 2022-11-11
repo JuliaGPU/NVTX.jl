cfbad605c585dacb4ea1f6df06145979afc822ff
Simon Byrne <simonbyrne@gmail.com>
2022-11-11 11:56:18 -0800

    try building with nvcc

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ using Libdl
 
 lib = "libjulia_nvtx_callbacks.$(Libdl.dlext)"
 try
-    run(`cc -fPIC -shared callbacks.c -lnvToolsExt -o $lib`)
+    run(`nvcc -Xcompiler -fPIC -shared callbacks.c -lnvToolsExt -o $lib`)
     @info "Success building $lib"
 catch e
     @warn "Could not build $lib"
