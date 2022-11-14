079ab5516320242353cb168b115b0200a14a6cf8
Simon Byrne <simonbyrne@gmail.com>
2022-11-14 12:45:36 -0800

    upload artifacts (#19)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
-filename = tempname()
 
-run(`nsys profile --output=$filename --export=json,sqlite --trace=nvtx $(Base.julia_cmd()) --project=$(Base.active_project()) --threads=3 basic.jl`)
-run(`nsys stats --report nvtxsum $(filename).sqlite`)
+if haskey(ENV, "GITHUB_WORKSPACE")
+    dirname = mkdir(joinpath(ENV["GITHUB_WORKSPACE"], "output"))
+else
+    dirname = mktempdir()
+end
 
+run(`nsys profile --output=$(joinpath(dirname, "basic")) --export=json,sqlite --trace=nvtx $(Base.julia_cmd()) --project=$(Base.active_project()) --threads=3 basic.jl`)
+run(`nsys stats --report nvtxsum $(joinpath(dirname, "basic.sqlite"))`)
